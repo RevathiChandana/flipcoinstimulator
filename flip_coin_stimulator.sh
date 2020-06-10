31b211dacd618f553019c0326826a8a5595c10bc
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-10 22:01:37 +0530

    uc-3:contine loop until either head or tail win 21 time

--- flip_coin_stimulator.sh
+++ flip_coin_stimulator.sh
@@ -3,21 +3,21 @@ echo "flipping a coin to display head or tail "
 head=0
 tail=1
 ((diff=tail-head+1))
-i=o;
+count=0;
 c_head=0
 c_tail=0
-while (( i<10 ))
+while [[ $c_head -ne 21 && $c_tail -ne 21 ]]
 do
 	result=$(( head+$((RANDOM%diff)) ))
-	echo "result : " $result
+	#echo "result : " $result
 	if [ $result -eq 0 ]
 	then
 		((c_head++))
 	else
 		((c_tail++))
 	fi
-((i++))
+((count++))
 done
 echo "number of times head won : " $c_head
 echo "number of times tail wom : " $c_tail
-
+echo "to win how many times does a coin flip : " $count
