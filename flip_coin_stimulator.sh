e8734c4095f57ef0298f1749ee2561f951abc0b5
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-10 21:47:59 +0530

    uc-2:show number of times head and tail has won

--- flip_coin_stimulator.sh
+++ flip_coin_stimulator.sh
@@ -1,8 +1,23 @@
-#! /bin/bash
+#! /bin/bash 
 echo "flipping a coin to display head or tail "
 head=0
 tail=1
 ((diff=tail-head+1))
-first_flip=$(( head+$((RANDOM%diff)) ))
-echo "first flip ( H=0 & T=1 ) : " $first_flip
+i=o;
+c_head=0
+c_tail=0
+while (( i<10 ))
+do
+	result=$(( head+$((RANDOM%diff)) ))
+	echo "result : " $result
+	if [ $result -eq 0 ]
+	then
+		((c_head++))
+	else
+		((c_tail++))
+	fi
+((i++))
+done
+echo "number of times head won : " $c_head
+echo "number of times tail wom : " $c_tail
 
